936caf52cf0091a088066a84d7a5bf5fce9d2cc0
Alejandro ML <amonteslevill@gmail.com>
2025-10-08 00:48:40 -0300

    Añade documentación de API, seguridad y página de inicio

--- drf/settings.py
+++ drf/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'primera_app',
     'rest_framework',
+    'drf_yasg'
     ]
 
 MIDDLEWARE = [
@@ -127,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
--- drf/urls.py
+++ drf/urls.py
@@ -2,8 +2,28 @@ from django.contrib import admin
 from django.urls import path, include
 from primera_app import views as app_views
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API de Biblioteca",
+      default_version='v1',
+      description="Documentación de la API para el proyecto de biblioteca.",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contacto@tuproyecto.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('primera_app.urls')),
     path('', app_views.pagina_inicio, name='home'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api-auth/', include('rest_framework.urls')),
 ]
